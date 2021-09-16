8a08e6daefd7aaa468b52f1fbb0e59cabe7671f0
abriyusif <abrisamana@gmail.com>
2021-09-16 10:15:26 +0300

    adding my personal website

---
+++ extJava.js
@@ -0,0 +1,8 @@
+function DRC(){
+  // alert(" This is the external javaScript file ");
+  var num1, num2, product;
+  num1 = 35;
+  num2 = 67;
+  product = num1 * num2;
+  console.log("The product of num1 and num2 is " + product);
+}
